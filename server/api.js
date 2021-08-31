8b859be58f0ae969443e5482d61f2e32214ba87d
Srihari <Srihari@zelarsoft.com>
2021-08-31 12:08:29 +0530

    ur

--- server/api.js
+++ server/api.js
@@ -12,7 +12,7 @@ app.listen(5000, ()=>{
 
 client.connect();
 app.get('/users', (req, res)=>{
-    client.query(`Select * from employees`, (err, result)=>{
+    client.query(`Select * from employee`, (err, result)=>{
         if(!err){
             res.send(result.rows);
         }
